5a8ea71a721731cd511b9a909646429d0d7e61fd
gustcustodio <gustcustodio@gmail.com>
2026-03-02 10:41:51 -0300

    create UrlRepository

---
+++ src/main/java/com/gustcustodio/url_shortening_service/repositories/UrlRepository.java
@@ -0,0 +1,12 @@
+package com.gustcustodio.url_shortening_service.repositories;
+
+import com.gustcustodio.url_shortening_service.entities.UrlEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UrlRepository extends JpaRepository<UrlEntity, Long> {
+
+
+
+}
