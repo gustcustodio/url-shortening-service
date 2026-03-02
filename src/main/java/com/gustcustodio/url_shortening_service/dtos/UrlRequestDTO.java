b4dcaad56652e52442f8ffdfbb760b55bbbfe812
gustcustodio <gustcustodio@gmail.com>
2026-03-02 18:04:01 -0300

    create UrlRequestDTO

---
+++ src/main/java/com/gustcustodio/url_shortening_service/dtos/UrlRequestDTO.java
@@ -0,0 +1,14 @@
+package com.gustcustodio.url_shortening_service.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UrlRequestDTO {
+
+    private String url;
+
+}
