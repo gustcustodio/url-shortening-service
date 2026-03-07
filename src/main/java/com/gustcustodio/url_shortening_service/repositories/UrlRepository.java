ebbcd3ce10e7bd22a2c968795d25be8deeb873b6
gustcustodio <gustcustodio@gmail.com>
2026-03-07 06:25:03 -0300

    add findByShortCode

--- src/main/java/com/gustcustodio/url_shortening_service/repositories/UrlRepository.java
+++ src/main/java/com/gustcustodio/url_shortening_service/repositories/UrlRepository.java
@@ -4,8 +4,12 @@ import com.gustcustodio.url_shortening_service.entities.UrlEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UrlRepository extends JpaRepository<UrlEntity, Long> {
+
+    Optional<UrlEntity> findByShortCode(String shortCode);
 
     boolean existsByShortCode(String shortCode);
 
